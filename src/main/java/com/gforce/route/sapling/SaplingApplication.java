62efba1ec03f5096fe5f8be809b7f288c27e4283
gforce <gforce_tian@outlook.com>
2019-05-22 21:13:56 +0800

    Initial commit

---
+++ src/main/java/com/gforce/route/sapling/SaplingApplication.java
@@ -0,0 +1,13 @@
+package com.gforce.route.sapling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaplingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gforce/route/sapling/SaplingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gforce.route.sapling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaplingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

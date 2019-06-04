fa6d77a81e72495f00588e04e598217f040f2bf8
gforce <448031739@qq.com>
2019-06-04 21:06:45 +0800

    edit threadPool

--- src/main/java/com/gforce/route/sapling/threadpool/SimpleThreadPool.java
+++ src/main/java/com/gforce/route/sapling/threadpool/SimpleThreadPool.java
@@ -1,5 +1,7 @@
 package com.gforce.route.sapling.threadpool;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,7 +10,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author tianyuan14
  * @create 2019/5/23 7:52 PM
  */
+@Slf4j
 public class SimpleThreadPool extends AbstractExecutorService {
+
+    private AtomicInteger workCount =  new AtomicInteger(0);
 
     private int corePoolSize;
 
@@ -20,14 +25,29 @@ public class SimpleThreadPool extends AbstractExecutorService {
 
     private BlockingQueue<Runnable> workQueue;
 
-    private AtomicInteger taskCount;
-
     public SimpleThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
         this.corePoolSize = corePoolSize;
         this.maximumPoolSize = maximumPoolSize;
         this.keepAliveTime = keepAliveTime;
         this.unit = unit;
         this.workQueue = workQueue;
+    }
+
+    class Worker implements Runnable{
+
+        private Runnable task;
+
+        private Thread thread;
+
+        public Worker(Runnable runnable) {
+            this.task = runnable;
+            this.thread = Executors.defaultThreadFactory().newThread(this);
+        }
+
+        @Override
+        public void run() {
+            runWorker(task);
+        }
     }
 
     @Override
@@ -56,13 +76,34 @@ public class SimpleThreadPool extends AbstractExecutorService {
     }
 
     @Override
-    public void execute(Runnable command) {
-        if (taskCount.get() < corePoolSize){
-            Thread thread = new Thread(command);
-            thread.start();
-        }else {
-            workQueue.add(command);
+    public void execute(Runnable runnable) {
+        if (workCount.get() < corePoolSize){
+            executeWorker(runnable);
         }
+        if (!workQueue.offer(runnable)){
+            throw new RejectedExecutionException();
+        }
+    }
 
+    private void executeWorker(Runnable runnable){
+        int wc = workCount.get();
+        while (workCount.compareAndSet(wc, wc + 1 )) {
+            Worker worker = new Worker(runnable);
+            Thread thread = worker.thread;
+            thread.start();
+        }
+    }
+
+    private void runWorker(Runnable runnable) {
+        while (workCount.get() <= corePoolSize) {
+            try {
+                while (runnable != null || (runnable = workQueue.take()) != null) {
+                    runnable.run();
+                    runnable = null;
+                }
+            }catch (InterruptedException e){
+                log.error("thread time out", e);
+            }
+        }
     }
 }
--- src/main/java/com/gforce/route/sapling/threadpool/StartTest.java
+++ src/main/java/com/gforce/route/sapling/threadpool/StartTest.java
@@ -1,13 +1,22 @@
 package com.gforce.route.sapling.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author tianyuan14
  * @create 2019/5/30 10:48 AM
  */
 public class StartTest {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> new Thread(() -> System.out.println(Thread.currentThread().getName())).start());
+        AtomicInteger atomicInteger = new AtomicInteger(0);
 
-        thread1.run();
+        ExecutorService es = new SimpleThreadPool(5,10, 1000,TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        for (int i = 0; i < 1000; i++) {
+            es.submit(() ->
+                    System.out.println(Thread.currentThread().getName() + " task:" + atomicInteger.incrementAndGet()));
+        }
     }
 }
